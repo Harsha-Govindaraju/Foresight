0babb6df14a49081a6bdc493394f3e46f3b17407
Harsha-Govindaraju <79359896+Harsha-Govindaraju@users.noreply.github.com>
2021-02-27 22:02:40 +0530

    Updating speech recognition api script

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const ctrl = new anycontrol();
+// ctrl.debug(true);
+ctrl.addCommand("next page", function () {
+  alert('Switching to next page');
+});
+
+ctrl.addCommand("previous page", function() {
+  alert('Switching to previous page');
+});
+ctrl.start();
 function textToAudio() {                
-                let speech = new SpeechSynthesisUtterance();
-                speech.lang = "en-US";
-                speech.text = 'hello';
-                speech.volume = 1;
-                speech.rate = 1;
-                speech.pitch = 1;
-                
-                window.speechSynthesis.speak(speech);
-            }
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = 'hello';
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
