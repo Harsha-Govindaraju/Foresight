a92900b81c88f1bc88b6e1b33d3842a1044fe0a4
Harsha-Govindaraju <79359896+Harsha-Govindaraju@users.noreply.github.com>
2021-03-21 21:43:04 +0530

    external url loading implementation

--- src/js/actionPanel.js
+++ src/js/actionPanel.js
@@ -1,8 +1,9 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 
-const ActionPanel = ({gapiReady, gapiSearch}) => {
-
+const ActionPanel = ({ gapiReady, gapiSearch, textToAudio, iframeSrc }) => {
+    const [iframeCont, setIframeCont] = useState(false);
     useEffect(() => {
+        let focusableEls = document.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
         if(!gapiReady && document.getElementById("gsc-i-id1")) document.getElementById("gsc-i-id1").disabled = true;
         if(gapiSearch) {
           document.getElementById("gsc-i-id1").value = gapiSearch;
@@ -10,14 +11,42 @@ const ActionPanel = ({gapiReady, gapiSearch}) => {
         }
     });
 
+    useEffect(() => { 
+      const ele = document.getElementsByClassName('iframeExternal');
+      if(iframeSrc && !iframeCont)  {
+        document.getElementById('iframeSrcBtn').click();
+        ele[0].contentWindow.focus();
+        setIframeCont(true);
+        let focusableEls = document.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+        focusableEls[0].focus();
+        const searchText = `Your result is ready and you can go back by pressing f5`;
+        textToAudio(searchText, 'gSearch');
+        setTimeout(() => {
+          //document.activeElement = null;
+          //document.activeElement = document.getElementById('externalPageIframe');
+          if (document.activeElement instanceof HTMLElement) document.activeElement.blur();
+          [...document.getElementsByTagName('iframe')].filter(a => a.classList.contains('iframeExternal'))[0].contentWindow.focus();
+          console.clear();
+          console.log('f after',document.activeElement);
+        }, 1000);
+
+        document.addEventListener('keydown', function(e) { 
+          if (e.key === 'Tab') { 
+            alert('hi')
+          }
+        });
+      } 
+    });
+
     return (
             <div className="col-md-8 col-xs-6">
               <h2>Action Panel</h2>
-              {/* <div className={`gcse-searchbox ${this.state.gapiReady ? 'showGoogleSearch': ''}`}></div><div className={`gcse-searchresults ${this.state.gapiReady ? 'showGoogleSearchResults': ''}`}></div> */}
-              <div className="gcse-searchbox"></div><div className="gcse-searchresults"></div>
-              {/* <object data={this.state.iframeSrc}></object> */}
-              {/* <a href={this.state.iframeSrc} dangerouslySetInnerHTML={{__html: `${this.state.iframeSrc ? `<object data=${this.state.iframeSrc} />` : ''}`}}></a> */}
-              {/* <iframe src={this.state.iframeSrc}></iframe> */}
+              <div className='gcse-searchbox'></div><div className="gcse-searchresults"></div>
+              {iframeSrc && <>
+                  <iframe src={iframeSrc} name="myFrame" className='iframeExternal' id='externalPageIframe'></iframe>
+                  <p><a href={iframeSrc} target="myFrame" id="iframeSrcBtn"></a></p>
+                  {/* <button className="backBtn">Back</button> */}
+              </>}
             </div>
     );
 }
--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import AnyControl from "./speechUtility";
 import Google from "./gapi";
 import ActionPanel from "./actionPanel";
 import DataUri from './dataUri';
+import { nodeName } from "jquery";
 
 const ctrl = new AnyControl();
 var utt = new SpeechSynthesisUtterance();
@@ -46,28 +47,140 @@ class App extends Component {
       s.parentNode.insertBefore(gcse, s);
     })();
     
+    // say.queue = [];
+    // //$(document).on("click mousemove", deQueue);
+    // //document.addEventListener("click mousemove", deQueue);
+    // ["click", "mousemove"].forEach(e => document.addEventListener(e, deQueue));
+    // say("hello");
+    
     //const ctrl = new AnyControl();
     console.log(ctrl);
     const _this = this;
     //ctrl.debug(true);
     ctrl.addCommand("google ${firstPhrase} ${secondPhrase} ${thirdPhrase}", function (ctx) {
       if(ctx.transcript.split("google")) {
-        _this.setState({ gapiSearch: ctx.transcript.split("google")[1].trim() });
+        _this.setState({ gapiSearch: ctx.transcript.split("google")[1].trim() }, () => _this.setFocusOnSearchResult());
       }
     });
     ctrl.start();
+
+    // window.addEventListener('keydown', function(event) {
+    //   if (event.key === "Tab") {
+    //       console.log(document.activeElement);
+    //   }
+    //   else if (event.key === 'Enter') {
+    //       alert('enter was pressed');
+    //   }
+    // }, true);
+    
+    document.addEventListener('keyup', function(e) {
+      var focusableEls = document.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+      var firstFocusableEl = focusableEls[0];  
+      var lastFocusableEl = focusableEls[focusableEls.length - 1];
+      let speech = new SpeechSynthesisUtterance();
+      speech.lang = "en-US";
+      speech.text = '';
+      speech.volume = 1;
+      speech.rate = 0.7;
+      speech.pitch = 1;
+      if (e.key === 'Tab') {
+        if ( e.shiftKey ) /* shift + tab */ {
+          if (document.activeElement === firstFocusableEl) {
+            lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else /* tab */ {
+          let anything_is_focused = (
+            document.hasFocus() &&
+            document.activeElement !== null &&
+            document.activeElement !== document.body &&
+            document.activeElement !== document.documentElement
+          );
+          //console.log(document.activeElement ,anything_is_focused);
+          let { type, nodeName, textContent, innerText } = document.activeElement;
+          if(anything_is_focused && (nodeName || type)) {
+            console.log(type, document.activeElement.nodeName, document.activeElement.textContent || document.activeElement);
+            //speech.text = textContent || innerText;
+            speech.text = _this.customTextGoogleSearch(document.activeElement);
+            window.speechSynthesis.speak(speech);
+            // switch(eleType) {
+            //   case 'text':
+            //     return '';
+            //   case 'button':
+            //     return '';
+            //   case 'submit':
+            //     return '';
+            // }
+            // if(eleType === "text" && document.activeElement.title === "search") {
+            //   speech.text = document.activeElement.title || document.activeElement.name;
+            //   window.speechSynthesis.speak(speech);
+            // } else if(eleType === "button") {
+            //   speech.text = document.activeElement.innerHTML;
+            //   window.speechSynthesis.speak(speech);
+            // }
+          }
+          // if (document.activeElement === lastFocusableEl) {
+          //   //console.log(document.activeElement, 'after');
+          //   firstFocusableEl.focus();
+          //   if(eleType === "button") {
+          //     speech.text = document.activeElement.innerHTML;
+          //     window.speechSynthesis.speak(speech);
+          //   }
+          //   e.preventDefault();
+          // }
+        }
+      }
+    });
+
+    document.addEventListener(`click`, e => {
+      const origin = e.target.closest("a");
+      
+      if (origin) {
+        console.clear();
+        console.log(`You clicked ${origin.href}`);
+        const url = origin.dataset.ctorig ? origin.dataset.ctorig :origin.href;
+        this.setState({ iframeSrc: url });
+        e.preventDefault();
+        return false;
+      }
+    });
   }
 
-  textToAudio = () => {
+  setFocusOnSearchResult = () => {
+    var focusableEls = document.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+  }
+
+  textToAudio = (text="Hey there, I am listening and please speak", gSearch='') => {
     let speech = new SpeechSynthesisUtterance();
     speech.lang = "en-US";
-    speech.text = "Hey there, I am listening and please speak"
+    speech.text = text;
     speech.volume = 1;
     speech.rate = 1;
     speech.pitch = 1;
 
+    // if(gSearch) {
+    //   document.getElementsByClassName('gsc-tabhActive')[0].click();
+    //   document.getElementsByClassName('gsc-tabhActive')[0].focus();
+    // }
+
     window.speechSynthesis.speak(speech);
   };
+
+  customTextGoogleSearch = (ele) => {
+    let { type, nodeName, textContent, innerText } = ele;
+        if(this.state.gapiSearch && nodeName === "A" && ele.classList.contains('gs-title')) {
+          return `You're on link ${textContent || innerText} and - press enter if you wish to visit the site`
+        } else if((nodeName === "BUTTON" || type === "button") && ele.classList.contains('gsc-search-button')) {
+          return `You're on ${textContent || innerText} button and - press enter if you wish to do`;
+        } else if((type === 'text' || nodeName === 'INPUT') && ele.classList.contains('gsc-input')) {
+          return `You're on search box and - please enter your input`
+        } else if(type === "button") {
+          return textContent || innerText
+        } else if (nodeName === "A" && textContent === "" && innerText === "" && ele.classList.contains('gs-image')) {
+          return `you're seeing an image with link - click enter to visit`
+        }
+        else return `You're on ${textContent || innerText}`;
+  }
 
   render() {
     return (
@@ -102,7 +215,7 @@ class App extends Component {
             </small>
           </ul>
         </div>
-        <ActionPanel gapiReady={this.state.gapiReady} gapiSearch={this.state.gapiSearch} />
+        <ActionPanel gapiReady={this.state.gapiReady} gapiSearch={this.state.gapiSearch} textToAudio={this.textToAudio} iframeSrc={this.state.iframeSrc} />
       </div>
         
       </>
