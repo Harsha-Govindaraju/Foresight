f2fda488c5691b4bdddf08ef0f966008e483b89d
Harsha-Govindaraju <79359896+Harsha-Govindaraju@users.noreply.github.com>
2021-04-02 20:44:44 +0530

    Delete src/src directory

--- src/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-
-import App from "./js/app";
-
-ReactDOM.render(<App />, document.getElementById("root"));
